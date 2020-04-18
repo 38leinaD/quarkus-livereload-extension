8ab2df2755e13fffae4288ea1e043bf19423f892
Daniel Platz <daniel.platz@me.com>
2020-04-18 09:14:52 +0200

    Improve reconnect when quarkus:dev reloads backend

--- runtime/src/main/resources/META-INF/resources/live-reload.js
+++ runtime/src/main/resources/META-INF/resources/live-reload.js
@@ -1,13 +1,49 @@
+const RECONNECT_BACKOFFS = [500, 1000, 1000, 1000, 1000, 1000, 2000, 2000, 2000, 5000];
+let currentBackoffIndex = 0;
 
 const host = location.host.split(':')[0];
 const port = location.host.split(':')[1];
 
-const ws = new WebSocket(`ws://${host}:${port}/live-reload`)
+let ws = null;
 
-// TODO: When websocket endpoint closes connection (e.g. live-reload of quarkus-backend), try to reconnect
-ws.addEventListener('message', function (event) {
-    console.log('live-reload-event from server', event);
-    location.reload();
-});
+function connectToLiveReload() {
 
-console.warn("live-reload activated")
+    try {
+        ws = new WebSocket(`ws://${host}:${port}/live-reload`)
+    }
+    catch (e) {
+        console.debug("error while connecting to websocket endpoint", e)
+        scheduleReconnect();
+        return;
+    }
+    ws.onmessage = (event) => {
+        console.log('live-reload triggerd by server', event);
+        location.reload();
+    };
+
+    ws.onopen = (event) => {
+        console.warn("live-reload activated")
+        currentBackoffIndex = 0;
+    };
+
+    ws.onclose = (event) => {
+        console.debug("live-reload websocket closed", event)
+        scheduleReconnect();
+    };
+
+    ws.onerror = (event) => {
+        console.debug("live-reload websocket error", event)
+    };
+}
+
+function scheduleReconnect() {
+    console.debug("Reconnect scheduled")
+    setTimeout(() => {
+        connectToLiveReload();
+    }, RECONNECT_BACKOFFS[currentBackoffIndex])
+
+    currentBackoffIndex = Math.min(currentBackoffIndex + 1, RECONNECT_BACKOFFS.length - 1) 
+}
+
+connectToLiveReload();
+
